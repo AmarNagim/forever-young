8ef7ba930d9510ac0cecdb01199bc9380bc48bd3
AmarNagim <99069493@mydavinci.nl>
2021-09-23 22:10:08 +0200

    level4

---
+++ robotarm-python-2021-main/levels/level4.py
@@ -0,0 +1,13 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 4')
+
+# Jouw python instructies zet je vanaf hier:
+for right in range(1):
+ robotArm.moveRight();
+
+
+
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
